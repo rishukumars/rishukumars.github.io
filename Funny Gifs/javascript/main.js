4c00fcb08d04480cf1447bfa80838726bb02c4cb
Rishu Kumar <rishukrajput453@gmail.com>
2023-07-26 01:52:37 +0530

    Find Funny Giffs project

---
+++ Funny Gifs/javascript/main.js
@@ -0,0 +1,88 @@
+//1.Grab the input from the search box(value)
+
+document.querySelector(".js-go").addEventListener('click',function () {
+ 
+var query = document.querySelector(".js-userinput").value;
+
+pushToDisplay(query);
+
+});
+// document.querySelector(".js-userinput").addEventListener('keyup',function (e) {
+
+
+// // If the key ENTER is pressed
+// if(e.key === 'Enter'){
+
+//     // var x = document.querySelector("input").value;
+
+
+// }
+
+    
+
+// });
+
+
+// //2.Manipulate the input data with Api
+
+// var getvalue = getValue();
+// console.log(getvalue);
+function pushToDisplay(query){
+    var url = "https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=lcZMsaCdOU3EQX3aRQzOKeTJNbXdiAxq";
+
+
+    //Make an HTTP request using XML HttpsRequest()
+    
+    const HttpRequest = new XMLHttpRequest();
+    HttpRequest.open('GET' , url)
+    HttpRequest.send();
+    
+    // console.log(HttpRequest);
+    
+    HttpRequest.addEventListener('load', function (e){
+    
+        var data = e.target.response;
+        pushToDOM(data);
+    
+    
+    
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // 3.Show GIFs they want
+    
+    function pushToDOM(input){
+     
+        var response = JSON.parse(input);
+    
+        var container = document.querySelector(".js-container");
+        //CHRIS forEach loop
+        var imageURLs = response.data;
+        imageURLs.forEach(function (image){
+            var one_image = image.images.fixed_height.url;
+            console.log(one_image);
+             
+        container.innerHTML = container.innerHTML + "<img src=\"" + one_image + "\">";
+        });
+        //My loop
+      
+        // var i=0;
+        // while(i<50){
+        //     var container = document.querySelector(".js-container");
+        //     var imageURL = response.data[i].images.fixed_height.url;
+            
+        // container.innerHTML = container.innerHTML + "<img src=\"" + imageURL + "\">";
+        //     i++;
+        // }
+    
+    }
+    
+}
